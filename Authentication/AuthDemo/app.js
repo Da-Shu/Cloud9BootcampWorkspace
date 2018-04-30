45697bce35f3f500eda5470bf9e4554809df44d0
da-shu <27363294+Da-Shu@users.noreply.github.com>
2018-04-30 12:23:13 +0000

    Auth

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -10,6 +10,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Argos is way too big",
     resave: false,
@@ -18,6 +19,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -29,11 +31,57 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 });
 
-//AUTH ROUTE
+//AUTH ROUTES
+
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handle user sign up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
+});
+
+//LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
+});
+
+//LOGOUT ROUTE
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING");
 });
